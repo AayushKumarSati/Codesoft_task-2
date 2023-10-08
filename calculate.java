577548102b6a5297a950b33a28c004b54f934217
Aayush Kumar Sati <128712771+AayushKumarSati@users.noreply.github.com>
2023-10-08 12:48:36 +0530

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,8 @@
+// int marks[]=new int [Subject1];
+
+            // System.out.println("Enter Marks In Out Of 100");
+            //     for (int i=0; i<Subject1;i++){
+            //         marks [i]=scn.nextInt();
+            //         total=total+marks[i];
+                // }
+                // int Total = Subject1 + Subject2 + Subject3 + Subject4 + Subject5;
